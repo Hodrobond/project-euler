ad5a16f63c4dd2f2620769c7f5f82c2cb0ad537c
Adam Kazberuk <hodrobond@gmail.com>
2017-06-14 13:57:38 -0700

    Using individual pages for questions now.

--- src/presentations/0001_0010.js
+++
@@ -1,5 +0,0 @@
-import * as solution1 from "../solutions/0001/solution.js";
-import * as solution2 from "../solutions/0002/solution.js";
-
-console.log(solution1.getSolution);
-console.log(solution2.getSolution);
